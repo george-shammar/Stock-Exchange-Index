b2a8f5c5ba036562f1df6c6e7c09a167b86bb695
george-shammar <george.gbenle@outlook.com>
2021-08-06 12:50:13 +0000

    Implement redux provider

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ const allStocksData = (
   fetch(`https://financialmodelingprep.com/api/v3/stock/list?apikey=${key}`)
     .then((response) => (response.json()))
     .then((response) => {
-      
+    
     })
 );
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './components/App';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+  </Provider>,
+  document.getElementById('root')
 );
