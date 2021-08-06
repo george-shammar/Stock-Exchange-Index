a0ef8823a759b91d656235d75a4d51f231cad7ba
george-shammar <george.gbenle@outlook.com>
2021-08-06 15:46:15 +0000

    Create favorite Button component

--- src/containers/FavoriteStocks.js
+++ src/containers/FavoriteStocks.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { removeStock } from '../actions/index';
-// import FavoriteButton from "../../components/FavoriteButton";
+import FavoriteButton from '../components/FavoriteButton';
 import Stock from '../components/Stock';
 const unfavoriteIcon = 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Broken_heart.svg/586px-Broken_heart.svg.png';
 
 const FavoriteStocks = () => {
-  // const favoriteRecipes = useSelector(selectFilteredFavoriteRecipes);
+  
   const dispatch = useDispatch();
 
   const onRemoveStockHandler = (stock) => {
