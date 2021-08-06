79d3d3479406d88881097c9226f4f51169bd7bd8
george-shammar <george.gbenle@outlook.com>
2021-08-06 18:12:08 +0000

    Add filter reducer to store

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import AllStocks from '../containers/AllStocks';
 import FavoriteStocks from '../containers/FavoriteStocks';
+import SearchTerm from '../containers/Search';
 
 const App = () => (
   <div>
+    <SearchTerm />
     <h2>Favorite Stocks</h2>
     <FavoriteStocks />
     <hr />
