bdb8c6f6822da39b4a4feec1288e725d1468aa41
george-shammar <george.gbenle@outlook.com>
2021-08-06 10:15:57 +0000

    Import provider from react-redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import {Provider} from 'react-redux';
 
 ReactDOM.render(
   <Provider store={store}>
