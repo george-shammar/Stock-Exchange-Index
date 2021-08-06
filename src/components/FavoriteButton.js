188a92169c0f3c193277f03b114477b33366f598
george-shammar <george.gbenle@outlook.com>
2021-08-06 17:14:22 +0000

    Fix linters

--- src/components/FavoriteButton.js
+++ src/components/FavoriteButton.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const FavoriteButton = ({ children, onClickHandler, icon }) => (
-  <button onClick={onClickHandler}>
+  <button type="button" onClick={onClickHandler}>
     <img className="heart-icon" alt="" src={icon} />
     {children}
   </button>
 );
+
+FavoriteButton.propTypes = {
+  children: PropTypes.string.isRequired,
+  onClickHandler: PropTypes.func.isRequired,
+  icon: PropTypes.string.isRequired,
+};
 
 export default FavoriteButton;
