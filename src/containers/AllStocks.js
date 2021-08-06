8290b25dbcf0508be6ea855b044ebfa7c236f97e
george-shammar <george.gbenle@outlook.com>
2021-08-06 15:19:59 +0000

    Add AllStocks component to App component

--- src/containers/AllStocks.js
+++ src/containers/AllStocks.js
@@ -1,1 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addStock, loadData } from '../actions/index';
+
+const AllStocks = () => {
+
+    const favoriteIcon = 'https://w7.pngwing.com/pngs/997/899/png-transparent-red-heart-illustration-heart-icon-3d-red-heart-love-heart-emoticon-thumbnail.png'
+    const dispatch = useDispatch();
+
+    const onFirstRender = () => {
+        dispatch(loadData());
+    }
+
+    useEffect(onFirstRender, []);
+
+    const onAddStockHandler = (stock) => {
+        dispatch(addStock(stock));
+    };
+
+    return (
+        <div>
+          {allStocks.map((stock) => (
+            <Stock stock={stock} key={stock.id}>
+              <FavoriteButton
+                onClickHandler={() => onAddStockHandler(stock)}
+                icon={favoriteIcon}
+              >
+                Add to Favorites
+              </FavoriteButton>
+            </Stock>
+          ))}
+        </div>
+      );
+}
+
+export default AllStocks;
