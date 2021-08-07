693fb83faf53d41c462e37cb0059eda872e9bbc9
george-shammar <george.gbenle@outlook.com>
2021-08-07 11:51:38 +0000

    Style stock component

--- src/containers/Search.js
+++ src/containers/Search.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { setSearchTerm, clearSearchTerm } from '../actions/index';
 import { selectSearchTerm } from '../reducers/filter';
+import '../Index.css';
 
 const searchIcon = 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Search_Icon.svg/768px-Search_Icon.svg.png';
 const clearIcon = 'https://cdn.iconscout.com/icon/premium/png-512-thumb/clear-2082112-1767251.png';
@@ -20,8 +21,8 @@ const SearchTerm = () => {
   };
 
   return (
-    <div>
-      <img alt="" src={searchIcon} />
+    <div id="search-container">
+      <img id="search-icon" alt="" src={searchIcon} />
       <input
         id="search"
         type="text"
@@ -33,6 +34,7 @@ const SearchTerm = () => {
         <button
           onClick={onClearSearchTermHandler}
           type="button"
+          id="search-clear-button"
         >
           <img src={clearIcon} alt="" />
         </button>
