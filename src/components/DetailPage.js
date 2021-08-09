fabd48d2e1e735ef4c48f85b6102ddc768af35d5
george-shammar <george.gbenle@outlook.com>
2021-08-09 21:00:45 +0000

    Create DetailPage component

---
+++ src/components/DetailPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DetailPage = () => {
+    return (
+        <div>
+            <h1>Stock Index Statistics</h1>
+        </div>
+    )
+}
+
+export default DetailPage;
