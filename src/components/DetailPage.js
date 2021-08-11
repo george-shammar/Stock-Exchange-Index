2a636cd1852310b783dfb6add821c70055357324
george-shammar <george.gbenle@outlook.com>
2021-08-11 19:44:12 +0000

    Fix bug with asynchronous call

--- src/components/DetailPage.js
+++ src/components/DetailPage.js
@@ -1,31 +1,37 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
-// import { detailStock } from '../api';
 /* eslint-disable */
-const DetailPage = () => {
-  const detailStock = async () => {
-      const response = await fetch('https://financialmodelingprep.com/api/v3/profile/AAPL?apikey=e517e7bdf90a7bed784312f134d9eab9');
-      const data = response.json();
-      // const x =JSON.parse(data);
-      return data;
+class DetailPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dossier: null,
     };
-    const stock = detailStock();
-    console.log(Object.keys(stock));
+  }
 
-  return (
-  <div>
-    <h1>Stock Index Statistics</h1>
-    {/* <h3>{stock.symbol}</h3>
-      <h2>{stock.name}</h2>
-      <h3>{stock.price}</h3>
-      <h2>{stock.exchange}</h2>
-      <h2>{stock.description}</h2> */}
-  </div>
-  )
-};
+  componentDidMount() {
+    fetch('https://financialmodelingprep.com/api/v3/profile/AAPL?apikey=e517e7bdf90a7bed784312f134d9eab9')
+      .then((resp) => resp.json())
+      .then((json) => this.setState({ dossier: json }));
+  }
 
-// DetailPage.propTypes = {
-//   onClickHandler: PropTypes.func.isRequired,
-//   stock: PropTypes.instanceOf(Object).isRequired,
-// };
+  render() {
+    const { dossier } = this.state;
+    return (
+      <div className="container">
+        {dossier !== null && dossier.length > 0
+          && (
+          <span>
+            {dossier.map((doss) => (
+              <span>
+                code:
+                {doss.symbol}
+              </span>
+            ))}
+          </span>
+          )}
+      </div>
+    );
+  }
+}
+
 export default DetailPage;
