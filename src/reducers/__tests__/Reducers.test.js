8107f52630f5c699ff1abb103458fc62bc7acfdd
george-shammar <george.gbenle@outlook.com>
2021-08-12 20:45:37 +0000

    Add negative test examples

--- src/reducers/__tests__/Reducers.test.js
+++ src/reducers/__tests__/Reducers.test.js
@@ -6,3 +6,17 @@ import { loadData, addStock, removeStock, selectAllStocks,
 test('should return the initial state', () => {  
     expect(allStocksReducer(undefined, {})).toEqual([]);
 })
+
+test('should return the initial state with empty array', () => {  
+    expect(allStocksReducer(undefined, {})).not.toEqual([{
+        symbol: 'SYM', name: 'Biscuits', price: '4.5', exchange: 'NYSE',
+      }]);
+})
+
+test('should return a defined initial state', () => {  
+    expect(allStocksReducer(undefined, {})).toBeDefined();
+})
+
+test('should return a truthy', () => {  
+    expect(allStocksReducer(undefined, {})).toBeTruthy();
+})
