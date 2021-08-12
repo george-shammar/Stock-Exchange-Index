2c3c9ede61faae7f1749172c16e500aa42f86117
george-shammar <george.gbenle@outlook.com>
2021-08-12 21:29:07 +0000

    Add unit tests for favoriteReducers

--- src/reducers/__tests__/allReducers.test.js
+++ src/reducers/__tests__/allReducers.test.js
@@ -1,7 +1,4 @@
 import allStocksReducer from '../allStocksReducer';
-import { loadData, addStock, removeStock, selectAllStocks,
-    selectFavoriteStocks, selectFilteredFavoriteStocks,
-    clearSearchTerm, setSearchTerm, selectFilteredAllStocks } from '../../actions/index';
 
 test('should return the initial state', () => {  
     expect(allStocksReducer(undefined, {})).toEqual([]);
--- src/reducers/__tests__/favoriteReducers.test.js
+++ src/reducers/__tests__/favoriteReducers.test.js
@@ -6,5 +6,27 @@ import { loadData, addStock, removeStock, selectAllStocks,
     test('should handle a stock being added to an empty favorite list', () => { 
         const stock = { symbol: 'SYM', name: 'Biscuits', price: '4.5', exchange: 'NYSE',}
         const previousState = []  
-        expect(favoriteStocksReducer(previousState, addStock(stock))).toEqual([{ symbol: 'SYM', name: 'Biscuits', price: '4.5', exchange: 'NYSE',}
+        expect(favoriteStocksReducer(previousState, addStock(stock))).toEqual([{ 
+            symbol: 'SYM', 
+            name: 'Biscuits', 
+            price: '4.5', 
+            exchange: 'NYSE',
+        }
     ])})
+
+    test('should handle the correct stock being added to an empty favorite list', () => { 
+        const stock = { symbol: 'SYM', name: 'Biscuits', price: '4.5', exchange: 'NYSE',}
+        const previousState = []  
+        expect(favoriteStocksReducer(previousState, addStock(stock))).not.toEqual([{ 
+            symbol: 'Hey', 
+            name: 'Apple', 
+            price: '1.2', 
+            exchange: 'NYSE',
+        }
+    ])})
+
+    test('should have a defined stock addition', () => { 
+        const stock = { symbol: 'SYM', name: 'Biscuits', price: '4.5', exchange: 'NYSE',}
+        const previousState = []  
+        expect(favoriteStocksReducer(previousState, addStock(stock))).toBeDefined();
+    });
