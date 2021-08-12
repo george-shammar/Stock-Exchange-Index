6204aa8f9a28645272a3d5bd720519c7d8d54335
george-shammar <george.gbenle@outlook.com>
2021-08-12 23:00:49 +0000

    Remove unneccessary import

--- src/reducers/__tests__/favoriteReducers.test.js
+++ src/reducers/__tests__/favoriteReducers.test.js
@@ -1,5 +1,5 @@
 import favoriteStocksReducer from '../favoritStocksReducer';
-import { loadData, addStock, removeStock } from '../../actions/index';
+import { addStock, removeStock } from '../../actions/index';
 
     const stock = { symbol: 'SYM', name: 'Biscuits', price: '4.5', exchange: 'NYSE',}
 
