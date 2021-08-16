2b5c313d85a6ce5c7f6f1cb1b1613cabb62acc0b
george-shammar <george.gbenle@outlook.com>
2021-08-16 03:15:08 +0000

    Refactor filter component to lift state from the parent component

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,33 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import AllStocks from '../containers/AllStocks';
 import FavoriteStocks from '../containers/FavoriteStocks';
 import SearchTerm from '../containers/Search';
+import { setSearchTerm, clearSearchTerm } from '../actions/index';
 import '../Index.css';
 
-const App = () => (
-  <div className="app">
-    <SearchTerm />
-    <h2>Watchlisted Stocks</h2>
-    <FavoriteStocks />
-    <hr />
-    <h2>All Stock Index</h2>
-    <AllStocks />
-  </div>
-);
+const App = () => {
+  const dispatch = useDispatch();
+
+  const onSearchTermChangeHandler = (e) => {
+    const userInput = e.target.value;
+    dispatch(setSearchTerm(userInput));
+  };
+
+  const onClearSearchTermHandler = () => {
+    dispatch(clearSearchTerm());
+  };
+
+  return (
+    <div className="app">
+      <SearchTerm onSearch={onSearchTermChangeHandler} onClear={onClearSearchTermHandler} />
+      <h2>Watchlisted Stocks</h2>
+      <FavoriteStocks />
+      <hr />
+      <h2>All Stock Index</h2>
+      <AllStocks />
+    </div>
+  );
+};
 
 export default App;
--- src/containers/Search.js
+++ src/containers/Search.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { setSearchTerm, clearSearchTerm } from '../actions/index';
+import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
 import { selectSearchTerm } from '../reducers/filter';
 import '../Index.css';
 
 const searchIcon = 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Search_Icon.svg/768px-Search_Icon.svg.png';
 const clearIcon = 'https://cdn.iconscout.com/icon/premium/png-512-thumb/clear-16-216586.png';
 
-const SearchTerm = () => {
+const SearchTerm = ({ onSearch, onClear }) => {
   const searchTerm = useSelector(selectSearchTerm);
-  const dispatch = useDispatch();
-
-  const onSearchTermChangeHandler = (e) => {
-    const userInput = e.target.value;
-    dispatch(setSearchTerm(userInput));
-  };
-
-  const onClearSearchTermHandler = () => {
-    dispatch(clearSearchTerm());
-  };
 
   return (
     <div className="search-container">
@@ -27,20 +17,30 @@ const SearchTerm = () => {
         id="search"
         type="text"
         value={searchTerm}
-        onChange={onSearchTermChangeHandler}
+        onChange={onSearch}
         placeholder="Filter Stocks"
       />
       {searchTerm.length > 0 && (
-        <button
-          onClick={onClearSearchTermHandler}
-          type="button"
-          className="search-clear-button"
-        >
-          <img className="clear" src={clearIcon} alt="" />
-        </button>
+      <button
+        onClick={onClear}
+        type="button"
+        className="search-clear-button"
+      >
+        <img className="clear" src={clearIcon} alt="" />
+      </button>
       )}
     </div>
   );
+};
+
+SearchTerm.propTypes = {
+  onSearch: PropTypes.func,
+  onClear: PropTypes.func,
+};
+
+SearchTerm.defaultProps = {
+  onSearch: null,
+  onClear: null,
 };
 
 export default SearchTerm;
